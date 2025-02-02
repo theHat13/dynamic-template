3fae26ad74b65d81b4c7811391371c8df7e3ddbf
RRR <regnier.raphael@gmail.com>
2025-02-02 15:05:22 +0100

    MAking hero dynamic

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,15 @@
 const { DateTime } = require("luxon");
 
 module.exports = function(eleventyConfig) {
+  // Copy static assets directory to output
   eleventyConfig.addPassthroughCopy('./src/assets');
+  // Copy main stylesheet to output
   eleventyConfig.addPassthroughCopy('./src/style.css');
+  // Copy admin interface files to output
   eleventyConfig.addPassthroughCopy('./src/admin');
-  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
-
+  // Add shortcode to display current year dynamically
+  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`); 
+  // Add filter to format post date
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
   });
