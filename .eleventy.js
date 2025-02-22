3e3c8b0085a9b4d536e2bc2716ba86242ba4af4b
charlotte-carpentier <carpentier.charlotte@live.fr>
2025-02-22 17:22:24 +0100

    test: Add typography.css

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,14 @@
 const { DateTime } = require("luxon");
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   // Copy static assets directory to output
   eleventyConfig.addPassthroughCopy('./src/assets');
-  // // Copy main stylesheet to output
-  // eleventyConfig.addPassthroughCopy('./style.css');
+  // Copy styles folder (tailwind-theme.css & typography.css)
+  eleventyConfig.addPassthroughCopy('./src/style');
   // Copy admin interface files to output
   eleventyConfig.addPassthroughCopy('./src/admin');
   // Add shortcode to display current year dynamically
-  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`); 
+  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
   // Add filter to format post date
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
